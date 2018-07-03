9ded3a634e0697135c10996dc2dfe6c05db10fdc
Talmage <utilitytitan@gmail.com>
2018-07-03 15:38:47 +0000

    built the first component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header =() =>{
+    return(
+        <div className="header">
+        
+        <h1>devcamp mad libs</h1>
+        <p>make a dank story</p>
+        </div>
+        )
+}
+export default Header;
