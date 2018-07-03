cdcb14c3e1a9bf98aea1eb425d384e9d3cf4b2e1
Talmage <utilitytitan@gmail.com>
2018-07-03 17:22:29 +0000

    I fixed an issue, and I made it so the text updates in real time

--- src/components/card.js
+++ src/components/card.js
@@ -8,18 +8,27 @@ class Card extends Component{
        super()
        
        this.state = {
-           color: '',
+           color: 'BLUE',
            pluralNoun: ''
        }
-       handleInputChange();
+       this.handleInputChange = this.handleInputChange.bind(this);
    }
    
-   handleInputChange(){
-       this.setState({color: 'red'})
+   handleInputChange(event){
+       
+       this.setState({color: event.target.value})
    }
    
    
-    render(){
+    render() {
+       
+        return(
+             <div className="card">
+             <h1>{this.state.color}</h1>
+             <input value={this.state.color} onChange={(event)=>this.handleInputChange(event)}/>
+             </div>
+            )
+        
         
         return(
             <div className="card">
