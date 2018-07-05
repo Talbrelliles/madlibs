cd9a1a36c8183bb4c6aba2efa3963930d2c9931a
Talmage <utilitytitan@gmail.com>
2018-07-05 17:44:41 +0000

    I added styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header =() =>{
     return(
         <div className="header">
-        
+        <div className="header__skew"></div>
+        <div className="header__border"></div>
         <h1>devcamp mad libs</h1>
         <p>make a dank story</p>
         </div>
